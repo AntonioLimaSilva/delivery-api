af36651d3cec7e42e4a393d898f20684c73942ce
Luciano Lima <luclimasilva@hotmail.com>
2020-03-22 12:45:31 -0300

    informacoes da api no swagger2

--- src/main/java/br/com/luciano/delivery/core/openapi/SpringFoxConfig.java
+++ src/main/java/br/com/luciano/delivery/core/openapi/SpringFoxConfig.java
@@ -2,9 +2,13 @@ package br.com.luciano.delivery.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,9 +21,10 @@ public class SpringFoxConfig implements WebMvcConfigurer {
     @Bean
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .build();
+            .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .build()
+            .apiInfo(apiInfo());
     }
 
     @Override
@@ -29,5 +34,14 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Delivery API")
+                .description("API aberta para clientes de restaurante")
+                .version("1.0.0")
+                .contact(new Contact("Luciano Lima", "www.umlima.com.br", "luclimasilva23@gmail.com"))
+                .build();
     }
 }

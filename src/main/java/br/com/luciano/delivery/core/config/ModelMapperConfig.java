79372a789743c3d776ddc6444b9f9b6047035f1d
Luciano Lima <luclimasilva@hotmail.com>
2020-03-23 09:05:41 -0300

    ajuste config bean validator no springfox

--- src/main/java/br/com/luciano/delivery/core/config/ModelMapperConfig.java
+++ src/main/java/br/com/luciano/delivery/core/config/ModelMapperConfig.java
@@ -6,11 +6,8 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 
 @Configuration
-@Import(BeanValidatorPluginsConfiguration.class) // integração com o bean validation do springfox, para adicionar required
 public class ModelMapperConfig {
 
     @Bean
